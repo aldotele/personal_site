d4704887fec4c06132df8a1d253dba33167de460
aldotele <aldo.telese@hotmail.it>
2022-11-26 18:06:10 +0100

    fixes typo in wsgi

--- portolio_conf/wsgi.py
+++ portolio_conf/wsgi.py
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portolio_conf.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portfolio_conf.settings')
 
 application = get_wsgi_application()
 app = application
